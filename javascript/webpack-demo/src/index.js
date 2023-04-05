c0f2a354f004af6a69bc8261dbecf2f651b75bbb
yehoki <patsenn2000@gmail.com>
2023-04-05 14:44:50 +0100

    Webpack output management

--- javascript/webpack-demo/src/index.js
+++ javascript/webpack-demo/src/index.js
@@ -1,22 +1,20 @@
 import _ from 'lodash';
 import myName from './myName';
-import './style.css';
-import Icon from './icon.svg';
+import printMe from './print.js';
 
 
 function component() {
     // const element = document.createElement('div');
     const myNameEl = document.createElement('div');
+    const btn = document.createElement('button');
+
 
     myNameEl.textContent = myName('NameHere');
-    myNameEl.classList.add('hello');
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-  
-    myNameEl.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
 
+    myNameEl.appendChild(btn);
 
     return myNameEl;
   
